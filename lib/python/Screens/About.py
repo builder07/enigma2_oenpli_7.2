e8bdeb01361c65d3feddeb5bfbf6574de4e1ae2e
builder07 <47144637+builder07@users.noreply.github.com>
2020-03-24 22:53:44 +0200

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -100,6 +100,14 @@ class About(Screen):
 		AboutText += hddinfo + "\n\n" + _("Network Info:")
 		for x in about.GetIPsFromNetworkInterfaces():
 			AboutText += "\n" + x[0] + ": " + x[1]
+			
+		AboutText += "\n"
+		AboutText += "\n"
+		
+		AboutText += _("Additional image information: ") + "\n"
+		AboutText += _("Idea: ") + about.getIdea() + "\n"
+		AboutText += _("E-mail: ") + about.getEmail() + "\n"
+		AboutText += _("Donate: ") + about.getDonate() + "\n"
 
 		self["AboutScrollLabel"] = ScrollLabel(AboutText)
 		self["key_green"] = Button(_("Translations"))
